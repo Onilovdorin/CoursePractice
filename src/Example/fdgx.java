17d4de607c78c7dd75d85a905531b9040be6301a
Dorinonilov <dorinonilov@gmail.com>
2022-02-07 20:24:15 +0200

    My homework commit

---
+++ src/Example/fdgx.java
@@ -0,0 +1,12 @@
+package Example;
+
+public class fdgx {
+    public static void main(String[] args) {
+
+      int myAge = 18;
+      String result;
+
+      result = myAge < 18 ? "Esti minor" : "Esti major";
+        System.out.println(result);
+    }
+}
---
+++ src/Lesson6homework/PrintNumbers.java
@@ -0,0 +1,10 @@
+package Lesson6homework;
+
+public class PrintNumbers {
+    public static void main(String[] args) {
+
+        for (int nr = 1; nr <= 10; nr++) {
+            System.out.println(nr);
+        }
+    }
+}
