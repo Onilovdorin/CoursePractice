f68f39aac386d6b7cdff2cbbbab9ab28e2b20a10
Dorinonilov <dorinonilov@gmail.com>
2022-02-20 15:24:41 +0200

    My homework commit

---
+++ src/lesson11/homework/SavingsAccount.java
@@ -0,0 +1,30 @@
+package lesson11.homework;
+
+public class SavingsAccount {
+
+    static private double annualInterestRate;
+    private double savingBalance;
+
+    public SavingsAccount() {
+
+    }
+
+    public SavingsAccount(double savingBalance) {
+        this.savingBalance = savingBalance;
+    }
+
+    public double getSavingBalance() {
+        return this.savingBalance;
+    }
+
+    public static void modifyInterestRate(double newInterestRate) {
+        annualInterestRate = newInterestRate;
+    }
+
+
+    public void calculateMonthlyInterest() {
+        double monthlyI;
+        monthlyI = (double) (this.savingBalance * annualInterestRate / 12);
+        this.savingBalance += monthlyI;
+    }
+}
