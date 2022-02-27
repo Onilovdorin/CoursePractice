63695ba2a121ac5bbe21aacac5f4ac38fb342ce4
Onilovdorin <98481010+Onilovdorin@users.noreply.github.com>
2022-02-27 23:38:03 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+package lesson12.homework.ex1;
+
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Customer customer1 = new Customer();
+        Customer customer2 = new Customer();
+        getAction(customer1);
+        getAction(customer2);
+        scanner.close();
+        Customer[] clients = {customer1, customer2};
+        System.out.println("Total profit:" + Customer.calculateTotalProfit(clients));
+    }
+
+    private static void getAction(Customer client) {
+        System.out.println("Welcome to our cheap garage park!");
+        System.out.print("How many hours do you want to park:");
+        int hours = scanner.nextInt();
+        client.setParkedHours(hours);
+        client.calculateCharges();
+        System.out.println("Customer fee: " + client.getClientCharge());
+    }
+}
